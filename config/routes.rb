9db57c09fb733efbecdb3e8d6a2150a1b08fc76d
kshio83 <okura0803@gmail.com>
2020-07-22 00:13:19 +0900

    フッター修正、ルート修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'questionnaires#index'
   get 'questionnaires/index'        =>  "questionnaires#index"  #アンケートトップ画面
   get 'questionnaires/new'          =>  "questionnaires#new"   #アンケート投稿画面
 end
