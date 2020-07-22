d3c2aad14f515b40059d3aa26bbe0a9f702bf229
kshio83 <okura0803@gmail.com>
2020-07-22 23:52:37 +0900

    view test

--- app/assets/javascripts/questionnaires.js
+++ app/assets/javascripts/questionnaires.js
@@ -1,2 +1,9 @@
+
 $('#textarea1').val('New Text');
 $('#textarea1').trigger('autoresize');
+
+$(document).ready(function() {
+  $('select').material_select();
+});
+
+$('select').material_select('destroy');
